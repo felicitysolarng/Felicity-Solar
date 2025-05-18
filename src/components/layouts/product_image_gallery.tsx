fa1a82981bed721a15402cdc31efe7bbffbfcd0e
joshua Oyewole <joshpp2013@gmail.com>
2025-05-18 18:28:21 +0100

    resolved bug on product details page and added responsiveness

--- src/components/layouts/product_image_gallery.tsx
+++ src/components/layouts/product_image_gallery.tsx
@@ -5,10 +5,10 @@ import React, { useState } from 'react'
 
 
 const imgs = [
-    "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHwxfHxoZWFkcGhvbmV8ZW58MHwwfHx8MTcyMTMwMzY5MHww&ixlib=rb-4.0.3&q=80&w=1080",
-    "https://images.unsplash.com/photo-1505751171710-1f6d0ace5a85?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHwxMnx8aGVhZHBob25lfGVufDB8MHx8fDE3MjEzMDM2OTB8MA&ixlib=rb-4.0.3&q=80&w=1080",
-    "https://images.unsplash.com/photo-1484704849700-f032a568e944?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw0fHxoZWFkcGhvbmV8ZW58MHwwfHx8MTcyMTMwMzY5MHww&ixlib=rb-4.0.3&q=80&w=1080",
-    "https://images.unsplash.com/photo-1528148343865-51218c4a13e6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHwzfHxoZWFkcGhvbmV8ZW58MHwwfHx8MTcyMTMwMzY5MHww&ixlib=rb-4.0.3&q=80&w=1080"
+    "/assets/images/product_images_1.jpeg",
+    "/assets/images/product_images_2.jpeg",
+    "/assets/images/product_images_3.jpeg", 
+    "/assets/images/product_images_4.jpeg", 
 ]
 function ProductImageGallery() {
     const [coverImg, setCoverImg] = useState(imgs[0])
@@ -21,6 +21,8 @@ function ProductImageGallery() {
             <Image
                 src={coverImg}
                 alt="Product"
+                width={540}
+                height={450}
                 className="w-full h-auto rounded-lg shadow-md mb-4" id="mainImage"
             />
             <div className="flex gap-4 py-4 justify-center overflow-x-auto">
