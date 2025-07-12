26a917cdd2090c4a888afa7732bf8c4084582577
joshua Oyewole <joshpp2013@gmail.com>
2025-07-12 15:29:17 +0100

    issue resolved successfullt

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,11 +7,6 @@ export function middleware(request: NextRequest) {
     const { pathname } = request.nextUrl;
 
     const token = request.cookies.get('token')?.value;
-
-    console.log(token);
-    console.log({pathname:pathname,token:token});
-    console.log(request);
-    
     
     if (protectedRoutes.some(route => pathname.startsWith(route)) && !token) {
         const loginUrl = new URL("/auth/login", request.url);
