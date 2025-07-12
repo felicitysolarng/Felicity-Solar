db3c3a715d537c698ff1a204e8106e061093638f
joshua Oyewole <joshpp2013@gmail.com>
2025-07-12 08:08:10 +0100

    corrected login error message

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -31,11 +31,12 @@ function Login() {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(variables),
-            credentials: 'include'
+            // credentials: 'include'
         });
+        console.log(res);
 
         if (!res.ok) {
-            throw new Error('Failed to send Quote');
+            throw new Error('Failed to Login');
         }
 
         const response = await res.json();
