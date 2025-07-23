57362c26dd79eca592bab6bf20492e350fd4e523
joshua Oyewole <joshpp2013@gmail.com>
2025-07-24 00:20:51 +0100

    added Google Adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,11 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
 
           gtag("config", "G-TTYZVHR9NE");`}
         </Script>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9169195841154004"
+          crossOrigin="anonymous"
+        />
       </head>
       <body className={`${inter.className} ${inter.variable} antialiased`}>
         <AOSInitializer />
