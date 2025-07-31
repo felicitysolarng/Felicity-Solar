3280c0e19cb27b0162dbf0e9292e2094ab718095
joshua Oyewole <joshpp2013@gmail.com>
2025-07-31 23:31:59 +0100

    changed startsWith /admins to /admin

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -7,10 +7,12 @@ export function middleware(request: NextRequest) {
 
   if (isAdminRoute) {
     const token = request.cookies.get("token")?.value;
+    const auth_token = request.cookies.get("auth-token")?.value;
     const isAdmin = request.cookies.get("is-admin")?.value;
+    console.log(`Token: ${token}, AuthToken is ${auth_token}, and Is Admin: ${isAdmin}`);
 
     // Block access if no token or not an admin
-    if (!token || isAdmin !== "true") {
+    if (!token || !auth_token || isAdmin !== "true") {
       return NextResponse.redirect(new URL("/auth/login", request.url));
     }
   }
