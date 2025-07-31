56f0c14fa1a892999854fd69cce064f275aa6c9b
joshua Oyewole <joshpp2013@gmail.com>
2025-07-31 17:17:06 +0100

    testing

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -9,6 +9,10 @@ export function middleware(request: NextRequest) {
     const token = request.cookies.get("auth-token")?.value;
     const isAdmin = request.cookies.get("is-admin")?.value;
 
+    console.log(request.cookies.getAll());
+
+    console.log(`Token: ${token}, Is Admin: ${isAdmin}`);
+
     // Block access if no token or not an admin
     if (!token || isAdmin !== "true") {
       return NextResponse.redirect(new URL("/auth/login", request.url));
