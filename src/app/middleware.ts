8af01a7e7bb24b175d646b9dbe5e54df61f7be7f
joshua Oyewole <joshpp2013@gmail.com>
2025-07-31 23:09:36 +0100

    renamed cookie from auth-token to token

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -6,12 +6,8 @@ export function middleware(request: NextRequest) {
   const isAdminRoute = request.nextUrl.pathname.startsWith("/admin");
 
   if (isAdminRoute) {
-    const token = request.cookies.get("auth-token")?.value;
+    const token = request.cookies.get("token")?.value;
     const isAdmin = request.cookies.get("is-admin")?.value;
-
-    console.log(request.cookies.getAll());
-
-    console.log(`Token: ${token}, Is Admin: ${isAdmin}`);
 
     // Block access if no token or not an admin
     if (!token || isAdmin !== "true") {
