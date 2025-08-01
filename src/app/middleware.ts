3e6e8ea8ad2b2d33f7f0e6b1ce2c433eb6a400ca
joshua Oyewole <joshpp2013@gmail.com>
2025-08-01 18:13:42 +0100

    t

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -7,12 +7,12 @@ export function middleware(request: NextRequest) {
 
   if (isAdminRoute) {
     const token = request.cookies.get("token")?.value;
-    const auth_token = request.cookies.get("auth-token")?.value;
-    const isAdmin = request.cookies.get("is-admin")?.value;
-    console.log(`Token: ${token}, AuthToken is ${auth_token}, and Is Admin: ${isAdmin}`);
+   // const auth_token = request.cookies.get("auth-token")?.value;
+   // const isAdmin = request.cookies.get("is-admin")?.value;
+    console.log(`Token: ${token}}`);
 
     // Block access if no token or not an admin
-    if (!token || !auth_token || isAdmin !== "true") {
+    if (!token) {
       return NextResponse.redirect(new URL("/auth/login", request.url));
     }
   }
