3a87311f2480fab9674a89d113ef542abc20e427
joshua Oyewole <joshpp2013@gmail.com>
2025-08-03 22:07:08 +0100

    reverting back to middleware

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,4 +1,5 @@
 // middleware.ts
+import { log } from "console";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
@@ -6,9 +7,11 @@ export function middleware(request: NextRequest) {
   const isAdminRoute = request.nextUrl.pathname.startsWith("/admin");
 
   if (isAdminRoute) {
-    const token = request.cookies.get("auth-token")?.value;
+    const token = request.cookies.get("token")?.value;
+    const token2 = request.cookies.get("auth-token")?.value;
     const isAdmin = request.cookies.get("is-admin")?.value;
 
+    log(`Middleware triggered for ${request.nextUrl.pathname}, token: ${token}, isAdmin: ${token2}, isAdmin: ${isAdmin}`);
     // Block access if no token or not an admin
     if (!token || isAdmin !== "true") {
       return NextResponse.redirect(new URL("/auth/login", request.url));
